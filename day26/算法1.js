bd76bc126f1e12469edc409ea54d6c40b174b6a3
CNlitai <2083645366@qq.com>
2020-08-21 09:08:33 +0800

    Update 算法1.js

--- day26/算法1.js
+++ day26/算法1.js
@@ -8,10 +8,31 @@
 //   return sum;
 // }
 
-function push(array, ...items) {
-  items.forEach(function (item) {
-    array.push(item);
-    console.log(item);
-  })
-}
-push(0, 1, 2, 3)
+// function push(array, ...items) {
+//   items.forEach(function (item) {
+//     array.push(item);
+//     console.log(item);
+//   })
+// }
+// push(0, 1, 2, 3)
+
+
+// var f = v => v;
+
+// var f = function(v) {
+//   return v;
+// }
+/**
+ *
+//  *
+//  * @param {*} resolve
+//  * @param {*} reject
+//  */
+// promise = new Promise(function(resolve, reject) {
+//   //................................................................
+//   if(kk){
+//     resolve(value);
+//   }else{
+//     reject(console.error();)
+//   }
+// })
