5f4191c3038ef6318d692bb55b0971aa7b3cc682
CNlitai <2083645366@qq.com>
2020-08-24 14:05:56 +0800

    Create demo.js

---
+++ day27/demo.js
@@ -0,0 +1,15 @@
+// var val = 'smtg';
+// console.log('Value is' + (val === 'smtg')?'Something':'Nothing');
+
+
+// for(var i =0; i < 10; i++) {
+//   setTimeout(function() {
+//     console.log(i);
+//   }, 1000);
+// }
+
+var i=100
+i++;
+var j=i++;
+var y =++j;
+console.log(y,j)
